439f9e67c578677f994408cb1118d253a23e6b11
carvsk <carvsk@LAPTOP-IJ1BED1T>
2019-09-30 00:20:14 +0100

    Primera versión, aún me da la excepción de unsupportedException

---
+++ src/kata1/Kata1.java
@@ -0,0 +1,13 @@
+package kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        
+        Person paco = new Person("Paquillo", new Date(98, 9, 18));
+        System.out.println(paco);
+    }
+    
+}
---
+++ src/kata1/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author carvsk
+ */
+public class Person {
+
+    Person(String paco_Juan, Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
