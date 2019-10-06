b4ecdcdfcf092d785cdee68bc1a70b764c60543a
carvsk <carvsk@LAPTOP-IJ1BED1T>
2019-10-06 20:49:49 +0100

    Repetición 1

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -13,8 +13,31 @@ import java.util.Date;
  */
 public class Person {
 
-    Person(String paco_Juan, Date date) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private final String name;
+    private final Date birthdate;
+    private static final int MS_PER_SECOND = 1000;
+    private static final int SECONDS_PER_MINUTE = 60;
+    private static final int MINUTES_PER_HOUR = 60;
+    private static final int HOURS_PER_DAY = 24;
+    private static final double DAYS_PER_YEAR = 365.25;
+    private static final double MS_PER_YEAR = MS_PER_SECOND *SECONDS_PER_MINUTE *MINUTES_PER_HOUR * HOURS_PER_DAY * DAYS_PER_YEAR;
+
+    public Person(String name, Date birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
     }
     
+    public int getAge()  {
+        return (int) ((new Date().getTime() - birthdate.getTime() ) / MS_PER_YEAR); 
+    }            
+    
+            
 }
